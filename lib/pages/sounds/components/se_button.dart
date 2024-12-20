a13625d51b180926cfa342ab51f5a761408aa61b
eita <>
2024-12-20 11:13:38 +0900

    randomを追加

--- lib/pages/sounds/components/se_button.dart
+++ lib/pages/sounds/components/se_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
 import '../logic/se_list.dart';
+//import 'dart:math';
 
 Widget soundButtonWidget(AudioPlayer audioPlayer) {
   return Column(
@@ -23,3 +24,29 @@ Widget soundButtonWidget(AudioPlayer audioPlayer) {
     }).toList(),
   );
 }
+
+/*Widget randomButtonWidget(AudioPlayer audioPlayer) {
+  final random = Random();
+  final randomOptions = List<SE>.from(SEList.seOptions)..shuffle(random);
+  final selectedOptions = randomOptions.take(2).toList();
+
+  return Column(
+    children: selectedOptions.map((button) {
+      return ElevatedButton(
+        onPressed: () async {
+          final index =
+              SEList.seOptions.indexWhere((se) => se.seid == button.seid);
+          if (index != -1) {
+            final path = SEList.getPath(index);
+            if (path != null) {
+              await audioPlayer.setAsset(path);
+              await audioPlayer.play();
+            }
+          }
+        },
+        child: Text(button.displayName),
+      );
+    }).toList(),
+  );
+}
+*/
