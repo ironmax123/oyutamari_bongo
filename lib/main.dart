6191effcea15ca4c4ce8ec5b68d820eb306814d3
eita <>
2024-12-26 19:55:55 +0900

    import修正

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:oyutamaribondo/pages/ble/ble_test.dart';
-import 'pages/sounds/sound_rp.dart';
-import 'package:oyutamaribondo/pages/home/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
